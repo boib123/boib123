ccc633da9bcce081bfbd0ffa4b98ac2b825193c5
boib123 <62674469+boib123@users.noreply.github.com>
2020-03-26 09:18:17 +1000

    This is a short game called hello world
    this is still in beta so of any bugs tell me.

---
+++ app.py
@@ -0,0 +1,100 @@
+print("created by Cooper Currin")
+print('Beta 0.1')
+d1a = input('this experience may make you feel uncomftable if yes exit this now. Continue? yes or no? ')
+if d1a == "yes":
+    print('great! ')
+if d1a == "no":
+    quit('gOoDbyE! ')
+print('**0********0000*******0**********0************00000000*******0********00********0******00000000******0000*********0**********00000************')
+print('**0********0**********0**********0***********0******0*********0******0**0******0******0*******0******0**0*********0**********0***0************')
+print('**0000*****0000*******0**********0**********0*****0************0****0****0****0******0******0********0000*********0**********0****0***********')
+print('**0**0*****0**********0**********0*********0*****0**************0**0******0**0******0******0*********0***0********0**********0****0***********')
+print('**0**0*****0000*******00000******00000*****000000****************00********00*******000000***********0****0*******00000******00000************')
+print('If the question has a [Y/N] box select with caps Y or N. If the question has A or B select one with caps on! ')
+rating = 10/10
+name = 'Cooper'
+is_published = True
+full_name = 'Phil Currin'
+age = 49
+is_new = True
+name = input('What is your name? ')
+colour = input('what is your favorite colour? ')
+print(name + ' thats a good colour '+ colour )
+print ('Hi ' + name )
+birth_year = input('Brith year:')
+print(type(birth_year))
+age = 2020 - int(birth_year)
+print(type(age))
+print(age)
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+print('ok then')
+car = input('what is your favorite car? ')
+print(car + " that's a good car ")
+gender = input('are you a female or male?')
+print('thats a good gender')
+man = input("so how's life been lately" )
+print('alright then')
+print('Question time!' )
+d1a = input("Do you want to: A) Approach the house. B) Approach the stable. [A/B]? : ")
+if d1a == "A":
+    print("You approach the cottage.")
+elif d1a == "B":
+    print("You approach the stables.")
+if d1a == "A":
+    quit('YoU dIe! ')
+if  d1a == "B":
+    print('you live!')
+if d1a == "C":
+    print('you found a secret 1/3')
+print('alright hopefully that was fun')
+d1a = input("was that fun?: A) YES!! B)no... [A/B]? ")
+if d1a == "A":
+    print('great! ')
+if d1a == "B":
+    print('oh ok then...')
+food = input('what is your favorite food? ')
+print( food + ' is a good food ')
+print('soooooooooooooooo')
+d1a = input("How many freinds do you have: A) A lot. B)a few. C)none")
+if d1a == "A":
+    print("That's great!")
+if d1a == "B":
+    print('thats awesome')
+if d1a =="C":
+    print('Lonely... how sad')
+d1a = input('Why are you a IDOIT? A) or B)? ')
+if d1a == "A":
+    print('You idoit ')
+if d1a == "B":
+    print('I knew it! ')
+book =input('what is your favorite book? ')
+print("that's a good book ")
+d1a = input('Am I in your house? A)yes B)yes ')
+if d1a == "A":
+    print("Correct! ")
+if d1a == "B":
+    print("That's right! ")
+d1a =input('where about in your house? A)in the corner.B)under your bed?')
+if d1a == "A":
+    print('yeS indeEd')
+if d1a == "B":
+    print('coRrecT! ')
+fun =input("What's The Worst Thing You've Ever Done In Your Life?")
+print('ok...')
+d1a =input('Are you alone? [Y/N]')
+if d1a == "Y":
+    print('thaTs rIghT!')
+if d1a == "N":
+    print('aRe you sUre?')
+d1a = input('do you like this experience? [Y/N]')
+if d1a == "Y":
+    print('great....')
+if d1a == "N":
+    quit('THen JuSt LAevE. ')
+if d1a == "i don't know":
+    print('you found a secret! 3/3')
+life =input('WhaT iS the meAninG of lIfE???')
+print('???????????????????????????????????????????????????????????????????????????????????????????????????????????????')
+quit("thank's for playing!")
